261658128aa8737629e22f96ffce793accb607fc
fixitdaz <fixitdaz@gmail.com>
2026-02-12 09:15:49 +0000

    made changes for plugin to work with gimp 3.0.8

--- src/paa-gimp-plugin.h
+++ src/paa-gimp-plugin.h
@@ -1,34 +1,26 @@
-﻿// paa-gimp-plugin.h : Include file for standard system include files,
-// or project specific include files.
+﻿#pragma once
 
-#pragma once
+/*
+ * author: fixitdaztv
+ * date: 2026-02-12
+ */
 
 #include <libgimp/gimp.h>
 #include <libgimp/gimpui.h>
 #include <grad_aff/paa/paa.h>
-
 #include <filesystem>
 #include <vector>
-#include <iostream>
+#include <string>
 
-#define PLUGIN_NAME "paa-gimp-plugin"
-#define LOAD_PROC "file-paa-load"
-#define SAVE_PROC "file-paa-save"
-#define LOAD_PAA_ERROR -1
-#define LOAD_PAA_CANCEL -2
+#define PLUG_IN_BINARY "paa-gimp-plugin"
+#define LOAD_PROC      "file-paa-load"
+#define SAVE_PROC      "file-paa-save"
 
-namespace fs = std::filesystem;
+/* Define GObject type for GIMP 3.0 plugin architecture */
+#define PAA_TYPE_PLUG_IN (paa_plug_in_get_type ())
+G_DECLARE_FINAL_TYPE (PaaPlugIn, paa_plug_in, PAA, PLUG_IN, GimpPlugIn)
 
-static void query (void);
-static void run (const gchar *name, gint nparams, const GimpParam  *param, gint *nreturn_vals, GimpParam **return_vals);
-
-gint32 loadPaa(const gchar* filename, int interactive);
-gboolean savePaa (const gchar *filename, gint32 imageId, gint32 drawableId, GError **error);
-
-const GimpPlugInInfo PLUG_IN_INFO =
-{
-    NULL,
-    NULL,
-    query,
-    run,
-};
+/* Logic prototypes */
+static GimpValueArray* paa_load(GimpProcedure* procedure, GimpRunMode run_mode, GFile* file, const GimpValueArray* args, gpointer data);
+static GimpValueArray* paa_save(GimpProcedure* procedure, GimpRunMode run_mode, GimpImage* image, gint n_drawables, GimpDrawable** drawables, GFile* file, const GimpValueArray* args, gpointer data);
+static bool is_power_of_two(uint32_t x);
